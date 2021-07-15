99d428efbd263d50efe88e28d0e85ec79673c0a2
Agnieszka Falandys <agnfal@gmail.com>
2021-07-15 19:13:44 +0200

    add props

---
+++ src/components/Column/Column.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './Column.scss';
+
+class Column extends React.Component{
+	static propTypes = {
+		titleColumn: PropTypes.string,
+	}
+	
+	render(){
+		return (
+			<section className={styles.component}>
+				<h3 className={styles.title}>{this.props.titleColumn}</h3>
+			</section>
+		)
+	}
+}
+
+export default Column;
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styles from './Hero.scss';
 
-const Hero = () => (
+const Hero = props => (
 	<header className={styles.component}>
-		<h2 className={styles.title}>Things-To-Do</h2>
-		<img className={styles.image} src="http://uploads.kodilla.com/bootcamp/fer/11.react/space.png"/>
+		<h2 className={styles.title}>{props.titleText}</h2>
+		<img className={styles.image} src={props.src}/>
 	</header>
 );
+
+Hero.propTypes = {
+  titleText: PropTypes.node.isRequired,
+  src: PropTypes.string,
+};
 
 export default Hero;
