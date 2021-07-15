f1571020bcf4d50859297f8ef582f84b252372ee
Agnieszka Falandys <agnfal@gmail.com>
2021-07-15 18:29:14 +0200

    init react project

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main className="app-content">
+        <h1>My first React app</h1>
+      </main>
+    )
+  }
+}
+
+export default App;
